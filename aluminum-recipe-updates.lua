a911cfd6c2d3be264f21e8ca9677417c16be5fd0
Brevven <brevingame@gmail.com>
2022-01-27 03:10:00 -0800

    more recipe tweaks

--- aluminum-recipe-updates.lua
+++ aluminum-recipe-updates.lua
@@ -5,17 +5,23 @@ local util = require("data-util");
 -- major changes
 util.multiply_recipe("sulfur", 2)
 util.replace_some_ingredient("sulfur", "petroleum-gas", 10, "alumina", 1)
-util.replace_ingredient("transport-belt", "iron-plate", "aluminum-plate")
 
+util.replace_ingredient("transport-belt", "iron-plate", "aluminum-plate")
+util.replace_ingredient("underground-belt", "iron-plate", "aluminum-plate")
+util.replace_ingredient("splitter", "iron-plate", "aluminum-plate")
+
+util.replace_ingredient("radar", "iron-plate", "aluminum-plate")
+util.replace_ingredient("steam-engine", "iron-plate", "aluminum-plate")
 util.replace_ingredient("storage-tank", "iron-plate", "aluminum-plate")
-util.remove_ingredient("engine-unit", "tungsten")
-util.add_ingredient("engine-unit", "spark-plug", 1)
+util.remove_ingredient("engine-unit", "tungsten-plate")
+util.add_ingredient("engine-unit", "spark-plug", mods["space-exploration"] and 2 or 1)
 util.add_ingredient("flamethrower", "spark-plug", 1)
 util.add_ingredient("flamethrower-turret", "spark-plug", 1)
 util.add_ingredient("tank", "spark-plug", 1)
 util.add_ingredient("tank", "spark-plug", 1)
 
 
+util.add_prerequisite("engine", "basic-alloys")
 
 
 -- "Mall" changes
@@ -34,7 +40,8 @@ util.replace_some_ingredient("tank", "steel-plate", 10, "alumina", 40)
 
 util.add_ingredient("kr-white-reinforced-plate", "alumina", 10)
 util.add_ingredient("kr-black-reinforced-plate", "alumina", 10)
-util.add_ingredient("transport-belt-loader", "alumina", 10)
+util.replace_ingredient("transport-belt-loader", "iron-plate", "aluminum-plate")
+util.add_ingredient("transport-belt-loader", "aluminum-plate", 10)
 
 
 
@@ -70,15 +77,19 @@ util.replace_some_ingredient("se-glass-vulcanite", "sand", 3, "alumina", 1)
 -- k2
 if mods.Krastorio2 then
   util.replace_some_ingredient("inserter-parts", "iron-stick", 2, "aluminum-plate", 1)
+  util.add_ingredient("inserter", "iron-stick", 1) -- nice symmetry
+  util.replace_ingredient("fast-inserter", "steel-plate", "aluminum-plate")
+  util.replace_ingredient("filter-inserter", "steel-plate", "aluminum-plate")
+  util.replace_ingredient("kr-wind-turbine", "iron-plate", "aluminum-plate")
 
   util.replace_ingredient("battery-equipment", "iron-plate", "aluminum-plate")
   util.replace_ingredient("night-vision-equipment", "iron-plate", "aluminum-plate")
   util.replace_ingredient("big-battery-equipment", "iron-plate", "aluminum-plate")
   util.replace_ingredient("personal-submachine-laser-defense-mk1-equipment", "iron-plate", "aluminum-plate")
 
-  util.replace_ingredient("additional-engine", "steel-plate", "aluminum-6601")
-  util.replace_ingredient("advanced-additional-engine", "steel-plate", "aluminum-6601")
-  util.replace_ingredient("vehicle-roboport", "steel-plate", "aluminum-6601")
+  util.replace_ingredient("additional-engine", "steel-plate", "aluminum-6061")
+  util.replace_ingredient("advanced-additional-engine", "steel-plate", "aluminum-6061")
+  util.replace_ingredient("vehicle-roboport", "steel-plate", "aluminum-6061")
 
 
   util.add_ingredient("kr-research-server", "aluminum-plate", 8)
@@ -103,11 +114,21 @@ if mods["aai-industry"] then
 end
 
 if mods["space-exploration"] then
+  util.replace_ingredient("se-adaptive-armour-equipment-1", "titanium-plate", "alumina")
+  util.replace_ingredient("se-adaptive-armour-equipment-1", "steel-plate", "alumina")
+
   util.replace_ingredient("se-space-transport-belt", "copper-plate", "aluminum-2219")
   util.add_ingredient("se-energy-beam-defence", "acsr-cable", 20)
+
+  util.replace_some_ingredient("se-rocket-launch-pad", "concrete", 500, "alumina", 500)
+  util.replace_some_ingredient("se-rocket-launch-pad", "steel-plate", 500, "aluminum-plate", 500)
+  util.add_ingredient("se-rocket-launch-pad", "spark-plug", 100)
+
 
   util.add_ingredient("se-experimental-alloys-data", "aluminum-plate", 1)
   util.add_to_product("se-experimental-alloys-data", "se-experimental-alloys-data", 1)
   util.add_to_product("se-experimental-alloys-data", "se-scrap", 1)
   util.add_to_ingredient("se-experimental-alloys-data", "se-empty-data", 1)
+
+  util.add_ingredient("se-electrical-shielding-data", "alumina", 1)
 end
