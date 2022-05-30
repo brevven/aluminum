9731e95e4de405dbcda63f1c6b88f3d76944c359
Brevven <brevingame@gmail.com>
2022-05-29 23:13:25 -0700

    large electric pole

--- aluminum-recipe-updates-power.lua
+++ aluminum-recipe-updates-power.lua
@@ -2,9 +2,12 @@ local util = require("data-util");
 
 -- most small electric poles handled in burner phase updates.
 
+-- this also handles modded "large-electric-pole" recipe for basic cases
+
 -- vanilla 
 util.replace_ingredient("medium-electric-pole", "copper-plate", "acsr-cable")
 util.replace_ingredient("big-electric-pole", "copper-plate", "acsr-cable")
+util.replace_ingredient("large-electric-pole", "copper-plate", "acsr-cable")
 util.replace_ingredient("substation", "copper-plate", "acsr-cable")
 util.add_ingredient("substation", "aluminum-plate", 4)
 
@@ -13,6 +16,7 @@ if mods["aai-industry"] or mods.Krastorio2 then
   util.add_prerequisite("medium-electric-pole", "reinforced-cable")
   util.replace_ingredient("medium-electric-pole", "copper-cable", "acsr-cable")
   util.replace_ingredient("big-electric-pole", "copper-cable", "acsr-cable")
+  util.replace_ingredient("large-electric-pole", "copper-cable", "acsr-cable")
   util.replace_ingredient("substation", "copper-cable", "acsr-cable")
 end
 
@@ -22,16 +26,19 @@ if mods["space-exploration"] then
   util.remove_product("se-recycle-small-electric-pole", "copper-cable")
   util.remove_product("se-recycle-medium-electric-pole", "copper-cable")
   util.remove_product("se-recycle-big-electric-pole", "copper-cable")
+  util.remove_product("se-recycle-large-electric-pole", "copper-cable")
   util.remove_product("se-recycle-substation", "copper-cable")
 
   util.add_product("se-recycle-small-iron-electric-pole", {"aluminum-cable", 1})
   util.add_product("se-recycle-small-electric-pole", {"aluminum-cable", 1})
   util.add_product("se-recycle-medium-electric-pole", {"acsr-cable", 1})
   util.add_product("se-recycle-big-electric-pole", {"acsr-cable", 2})
+  util.add_product("se-recycle-large-electric-pole", {"acsr-cable", 4})
   util.add_product("se-recycle-substation", {"acsr-cable", 4})
 end
 
 -- final amounts apply to all mod loadouts
 util.set_ingredient("medium-electric-pole", "acsr-cable", 1)
 util.set_ingredient("big-electric-pole", "acsr-cable", 2)
+util.set_ingredient("large-electric-pole", "acsr-cable", 4)
 util.set_ingredient("substation", "acsr-cable", 4)
