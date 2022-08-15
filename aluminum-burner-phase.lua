681a88575d46c0bd25b3aa690a7d20632bafe4e3
Brevven <brevingame@gmail.com>
2022-08-14 19:51:19 -0700

    notes

--- aluminum-burner-phase.lua
+++ aluminum-burner-phase.lua
@@ -97,7 +97,11 @@ util.add_prerequisite("heavy-armor", "copper-processing")
 util.set_enabled("deadlock-copper-lamp", false)
 util.add_effect("copper-processing", { type = "unlock-recipe", recipe = "deadlock-copper-lamp" })
 
+-- All customizations or custom unlocks prior to this block
 util.add_unlock_force("electronics", "electronic-circuit")
 util.replace_ingredients_prior_to("electronics", "electronic-circuit", "aluminum-cable")
 util.replace_ingredients_prior_to("copper-processing", "copper-cable", "aluminum-cable")
 util.replace_ingredients_prior_to("copper-processing", "copper-plate", "aluminum-plate")
+-- Any post-fixes (eg aluminum back to copper) after this block
+
+-- End of file
