a714bc6a011a03293cae6524ff6da559b66295c5
Brevven <brevingame@gmail.com>
2023-01-16 18:03:55 -0800

    update

--- compatibility/crafting-efficiency.lua
+++ compatibility/crafting-efficiency.lua
@@ -7,7 +7,9 @@ if mods["crafting-efficiency-2"] then
 
   for i, recipe in pairs(util.me.recipes) do
     -- add remaining recipes with direct items to find the icons
-    if data.raw.recipe[recipe] and data.raw.item[recipe] and not eff_data[recipe] and util.get_result_count(recipe) == 1 then
+    if data.raw.recipe[recipe] and data.raw.item[recipe] and not eff_data[recipe] and 
+    recipe ~= "alumina" and -- for some reason this does not work, despite having a main_product
+    (data.raw.recipe[recipe].main_product or util.get_result_count(recipe) == 1) then
       eff_data[recipe] = {
         max = 15,
         icon =  data.raw.item[recipe].icon,
